d67af794a1d5decdb3f1f4ce495b9e4e447a0371
KEENERVL20 <keenervl20@gcc.edu>
2022-03-08 15:46:07 -0500

    User Class

---
+++ ScheduleGnome/src/User.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class User {
+    ArrayList<Schedule> savedSchedules;
+
+    public User(String username, String password){
+
+    }
+
+    void compareSchedules(ArrayList<Schedule> schedules){
+
+    }
+
+   void createAccount(){
+
+    }
+
+    void deleteAccount(){
+
+    }
+
+
+
+}
