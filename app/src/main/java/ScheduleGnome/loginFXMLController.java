cd7048a7fb37c9c0ec25c2bf473b487ec291ab7a
barnesge19 <barnesge19@gcc.edu>
2022-03-24 13:34:02 -0400

    First iteration of login persisting

--- app/src/main/java/ScheduleGnome/loginFXMLController.java
+++ app/src/main/java/ScheduleGnome/loginFXMLController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class loginFXMLController {
@@ -13,9 +15,14 @@ public class loginFXMLController {
     @FXML private Text actiontarget;
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
+    private String usersFileName = "users.txt";
     
     @FXML protected void loginButton(ActionEvent event) throws IOException {
         if(usernameField.getText().isBlank() || passwordField.getText().isBlank()) return;
+
+        //I should add all existing users from the usersFile to JavaFXApp.addUser i think
+
+        
         int response = JavaFXApp.login(usernameField.getText(), passwordField.getText());
         switch (response) {
             case -1:
@@ -38,12 +45,52 @@ public class loginFXMLController {
     @FXML protected void registerButton(ActionEvent event) throws IOException {
         if(usernameField.getText().isBlank() || passwordField.getText().isBlank()) return;
 
-        User newUser = new User(usernameField.getText(), passwordField.getText());
-        JavaFXApp.addUser(newUser);
-        JavaFXApp.changeScene("savedScene.fxml");
+        //Storing passwords with plain text encryption algorithm
+        String newUsername = usernameField.getText();
+        String newPassword = passwordField.getText();
+
+        if(registerUser(newUsername, newPassword)) {
+            User newUser = new User(newUsername, newPassword);
+            JavaFXApp.addUser(newUser);
+            JavaFXApp.changeScene("savedScene.fxml");
+        }
+        else {
+            //Is this how I do this?
+            //How do i display the message from earlier
+            actiontarget.setText("Registration failed.");
+        }
+
+        
 //        JavaFXApp.setScene("select");
 
         //actiontarget.setText("Logged in as " + JavaFXApp.getCurrentUser().getUsername());
+    }
+
+    /**
+     * Registers the new user in the users file
+     * @param username username of the new user
+     * @param password password of the new user
+     * @return true on success, false on error
+     */
+    protected boolean registerUser(String username, String password) {
+        try {
+            File usersFile = new File(usersFileName);
+            FileWriter fw = new FileWriter(usersFile);
+
+            if(username.contains(":")) {
+                fw.close();
+                throw new Exception("Username cannot contain the character \':\'");
+            }
+
+            fw.write(username + ":" + password + "\n");
+            fw.flush();
+            fw.close();
+            return true;
+        }
+        catch (Exception e) {
+            //Log the exception or display it in javafx app
+            return false;
+        }
     }
 
 }
