e86f455bf96578fa3361f9b22a5fdd85329a8aa8
tkeener01 <keenervl20@gcc.edu>
2022-03-26 15:39:40 -0400

    Select a saved schedule to edit

--- app/src/main/java/ScheduleGnome/savedFXMLController.java
+++ app/src/main/java/ScheduleGnome/savedFXMLController.java
@@ -1,44 +1,25 @@
 package ScheduleGnome;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
-
-import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class savedFXMLController implements Initializable {
-     User user = JavaFXApp.getCurrentUser();
-//    ObservableList list = FXCollections.observableArrayList();
 
 
-//    private ListView<String> savedList;
     @FXML
-    private TextField textField;
+    private ListView<String> listView;
     @FXML
-    private Button button;
-    @FXML
-    private Label greetingLabel;
+    private ArrayList<String> savedNames;
 
 
-
-
-//    public void create() throws IOException{
-//        user.addNewSchedule(newName.getText());
-//        JavaFXApp.changeScene("searchScheduleScene.fxml");
-//    }
     @FXML
     public void clickNew(ActionEvent actionEvent) throws IOException{
         JavaFXApp.changeScene("nameSchedule.fxml");
@@ -48,31 +29,23 @@ public class savedFXMLController implements Initializable {
 
     @Override
     public void initialize (URL url, ResourceBundle resourceBundle) {
-//        loadData();
+        savedNames = new ArrayList<>();
+        savedNames.addAll(JavaFXApp.getCurrentUser().savedSchedules.keySet());
+        listView.getItems().addAll(savedNames);
+
+        listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                try {
+                    JavaFXApp.changeScene("searchScheduleScene.fxml");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
-//    private void loadData(){
-//        list.removeAll(list);
-//        ObservableList<String> items = FXCollections.observableArrayList(list);
-//        int i = 1;
-//        for (String key : user.getSavedSchedules().keySet()) {
-//            list.add(key);
-//            i++;
-//        }
-//       savedList.getItems().addAll(list);
-//    }
-
-
-
-//    public void selectSchedule(ActionEvent actionEvent){
-//
-//    }
-
-
-
-//    public void deleteSchedule(ActionEvent actionEvent){
-//        JavaFXApp.getCurrentUser().deleteSchedule();
-//    }
 
 
 
@@ -80,15 +53,9 @@ public class savedFXMLController implements Initializable {
     public void logout(ActionEvent actionEvent) throws IOException {
         JavaFXApp.changeScene("loginScene.fxml");
 
-
     }
 
-    public void newSchedule(ActionEvent actionEvent)throws IOException{
-        JavaFXApp.setCurrentSchedule(new Schedule("" + java.lang.Math.random()));
-        Pane pane = new Pane();
 
-
-    }
 
 
 
