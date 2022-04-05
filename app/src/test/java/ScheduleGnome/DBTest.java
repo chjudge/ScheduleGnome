5b94a723f4733c5dc3f5d85607cd52aa3a13bba5
chjudge <63122915+chjudge@users.noreply.github.com>
2022-04-05 16:16:54 -0400

    added test for gnomedb connection

---
+++ app/src/test/java/ScheduleGnome/DBTest.java
@@ -0,0 +1,28 @@
+package ScheduleGnome;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DBTest {
+
+
+
+    @Test
+    void connect() throws SQLException {
+        final String USERNAME = "schedulegnome";
+        final String PASSWORD = "gnomepassword";
+        final String CONNECTION_STRING = "jdbc:mysql://108.52.164.126:3306/gnomedb";
+        Properties props = new Properties();
+        props.put("user", USERNAME);
+        props.put("password", PASSWORD);
+        Connection conn = DriverManager.getConnection(CONNECTION_STRING, props);
+
+        assertEquals(CONNECTION_STRING, conn.getMetaData().getURL());
+    }
+}
